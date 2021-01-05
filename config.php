e1642506a6a653f6636fd810f1f4a9db45252e11
Cawko <zaragosa666@gmail.com>
2021-01-05 13:38:55 +0200

    MVC adn

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die ('False');
+
+const SITE_URL = 'http://shop';
+const PATH ='/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = 'root';
+const DBNAME = 'shop';
